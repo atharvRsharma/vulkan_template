795e1eadcf08074dc91868dbf20bf80fc61ff4d4
atharv sharma <atharv1908@gmail.com>
2026-02-19 15:30:30 +0530

    validation layers

--- main.cpp
+++ main.cpp
@@ -13,6 +13,18 @@ import vulkan_hpp;
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
+const std::vector<char const*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
+
+
 class HelloTriangleApplication {
 public:
     void run() {
@@ -24,6 +36,8 @@ public:
 
 private:
     GLFWwindow* window = nullptr;
+    vk::raii::Context   context;
+    vk::raii::Instance  instance = nullptr;
 
     void initWindow() {
         glfwInit();
@@ -33,8 +47,55 @@ private:
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "sug", nullptr, nullptr);
     }
-    void initVulkan() {
 
+    void createInstance() {
+        constexpr vk::ApplicationInfo appinfo{ .pApplicationName = "huh",
+                      .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+                      .pEngineName = "no engine",
+                      .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+                      .apiVersion = vk::ApiVersion14 };
+
+        std::vector<char const*> requiredLayers;
+        if (enableValidationLayers) {
+            requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+        }
+
+        auto layerProperties = context.enumerateInstanceLayerProperties();
+
+        if (std::ranges::any_of(requiredLayers, [&layerProperties](auto const& requiredLayer) {
+            return std::ranges::none_of(layerProperties, [requiredLayer](auto const& layerProperty) {
+                return strcmp(layerProperty.layerName, requiredLayer) == 0;
+                });
+            }))
+        {
+            throw std::runtime_error("atleast 1 req layers aint supported twin");
+        }
+
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i{}; i < glfwExtensionCount; ++i) {
+            if (std::ranges::none_of(extensionProperties,
+                [glfwExtension = glfwExtensions[i]](auto const& extensionProperty) {
+                    return strcmp(extensionProperty.extensionName, glfwExtension) == 0;
+                }))
+            {
+                throw std::runtime_error("required glfw extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+
+        vk::InstanceCreateInfo createInfo{
+            .pApplicationInfo = &appinfo,
+            .enabledExtensionCount = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions};
+
+        instance = vk::raii::Instance(context, createInfo);
+    }
+
+    void initVulkan() {
+        createInstance();
     }
 
     void mainLoop() {
